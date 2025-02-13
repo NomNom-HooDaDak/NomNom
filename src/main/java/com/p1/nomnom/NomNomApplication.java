b57a2ac57458f972150e67165aa4673123210531
poorm <315luz10@gmail.com>
2025-02-13 14:15:17 +0900

    [feat] init commit

---
+++ src/main/java/com/p1/nomnom/NomNomApplication.java
@@ -0,0 +1,13 @@
+package com.p1.nomnom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomNomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NomNomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/p1/nomnom/NomNomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p1.nomnom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomNomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
