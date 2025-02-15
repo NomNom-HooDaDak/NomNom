5c19dfce0fd456f3043207f2b087ebfdd56d74a3
poorm <315luz10@gmail.com>
2025-02-15 17:25:40 +0900

    [refactor] orderItem 엔티티 수정
    - nullable 옵션 통일
    - food와 조인 해제 결정
    - order와 조인 결정
    - order 연관관계 매핑 메서드 작성

--- src/main/java/com/p1/nomnom/orderItems/entity/OrderItem.java
+++ src/main/java/com/p1/nomnom/orderItems/entity/OrderItem.java
@@ -16,28 +16,23 @@ public class OrderItem extends BaseEntity {
     @Column(name = "order_item_id")
     private String id = NanoIdUtils.randomNanoId();
 
-    /*
-    Food는 여러 개의 OrderItem에서 참조할 수 있지만, OrderItem은 반드시 하나의 Order에 속해야 함.
-    order의 일부이기 때문에 엔티티 직접 연결
-     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id", nullable = false)
-    @NotNull
     private Order order;
 
-    /*
-    OrderItem의 역할은 "주문된 상품 정보만 저장하는 것"
-    Food 엔티티를 가져오면 OrderItem이 Food에 종속됨 → Food 삭제 시 OrderItem도 영향을 받을 수 있음.
-     */
     @Column(name = "food_id", nullable = false)
-    @NotNull
     private String foodId;
 
     @Column(nullable = false)
-    @NotNull
     private int quantity;
 
     @Column(nullable = false)
-    @NotNull
     private int price;
+
+    public void setOrder(Order order) {
+        this.order = order;
+        if (order != null && !order.getOrderItems().contains(this)) {
+            order.getOrderItems().add(this);
+        }
+    }
 }
