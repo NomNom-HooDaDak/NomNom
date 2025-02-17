894b48132c4f126a541afd50ffd492415fc97f85
kwon2501 <kwonkyulin25@gmail.com>
2025-02-17 23:17:35 +0900

    [feat] AI generatedDescription 구현

--- src/main/java/com/p1/nomnom/ai/entity/Ai.java
+++ src/main/java/com/p1/nomnom/ai/entity/Ai.java
@@ -2,6 +2,7 @@ package com.p1.nomnom.ai.entity;
 
 import com.p1.nomnom.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,9 +19,11 @@ public class Ai extends BaseEntity {
     private UUID id;
 
     @Column(name = "question", nullable = false, columnDefinition = "TEXT")
+    @Size(max = 500, message = "질문은 최대 500자까지 입력 가능합니다.")
     private String question;
 
     @Column(name = "answer", nullable = false, columnDefinition = "TEXT")
+    @Size(max = 255, message = "답변은 최대 255자까지 입력 가능합니다.")
     private String answer;
 
     @Column(name = "food_name", nullable = false, columnDefinition = "TEXT")
