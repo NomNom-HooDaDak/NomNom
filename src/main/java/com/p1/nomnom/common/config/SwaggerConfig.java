4808966799a5dd635eed1644bbf0f7084059c661
poorm <315luz10@gmail.com>
2025-02-19 00:17:04 +0900

    [feat] 스웨거 적용

---
+++ src/main/java/com/p1/nomnom/common/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.p1.nomnom.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NomNom API")
+                        .description("NomNom API 문서")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Public API")
+                .pathsToMatch("/api/**") // `/api/` 경로만 Swagger에 표시
+                .build();
+    }
+}
