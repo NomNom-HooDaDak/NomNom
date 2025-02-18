f8f830fe4642963bd6e2ff48b68d587dbe8d571e
kwon2501 <kwonkyulin25@gmail.com>
2025-02-18 22:09:27 +0900

    [feat] CategoryController, StoreController 권한 추가

--- src/main/java/com/p1/nomnom/store/controller/StoreController.java
+++ src/main/java/com/p1/nomnom/store/controller/StoreController.java
@@ -1,8 +1,10 @@
 package com.p1.nomnom.store.controller;
 
+import com.p1.nomnom.security.aop.RoleCheck;
 import com.p1.nomnom.store.dto.request.StoreRequestDTO;
 import com.p1.nomnom.store.dto.response.StoreResponseDTO;
 import com.p1.nomnom.store.service.StoreService;
+import com.p1.nomnom.user.entity.UserRoleEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +21,8 @@ public class StoreController {
 
     private final StoreService storeService;
 
-    // 가게 등록
+    // 가게 등록, 가게 주인은 못함
+    @RoleCheck({UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @PostMapping
     public ResponseEntity<StoreResponseDTO> createStore(@Valid @RequestBody StoreRequestDTO storeRequestDTO) {
         StoreResponseDTO storeResponseDTO = storeService.createStore(storeRequestDTO);
@@ -27,6 +30,7 @@ public class StoreController {
     }
 
     // 가게 정보 수정
+    @RoleCheck({UserRoleEnum.OWNER, UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @PatchMapping("/{storeId}")
     public ResponseEntity<StoreResponseDTO> updateStore(@PathVariable UUID storeId, @Valid @RequestBody StoreRequestDTO storeRequestDTO) {
         StoreResponseDTO storeResponseDTO = storeService.updateStore(storeId, storeRequestDTO);
@@ -34,6 +38,7 @@ public class StoreController {
     }
 
     // 특정 가게 조회
+    @RoleCheck({UserRoleEnum.CUSTOMER, UserRoleEnum.OWNER, UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @GetMapping("/{storeId}")
     public ResponseEntity<StoreResponseDTO> getStore(@PathVariable UUID storeId) {
         StoreResponseDTO storeResponseDTO = storeService.getStore(storeId);
@@ -41,6 +46,7 @@ public class StoreController {
     }
 
     // 카테고리별 가게 조회 + 검색, 정렬, 페이지네이션
+    @RoleCheck({UserRoleEnum.CUSTOMER, UserRoleEnum.OWNER, UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @GetMapping("/category")
     public ResponseEntity<List<StoreResponseDTO>> getStoresByCategory(
             @RequestParam(value = "category_id") UUID categoryId,
@@ -58,6 +64,7 @@ public class StoreController {
     }
 
     // 모든 가게 조회 + 검색, 정렬, 페이지네이션
+    @RoleCheck({UserRoleEnum.CUSTOMER, UserRoleEnum.OWNER, UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @GetMapping
     public ResponseEntity<List<StoreResponseDTO>> getAllStores(
             @RequestParam(value = "page", defaultValue = "0") int page,
@@ -74,6 +81,7 @@ public class StoreController {
     }
 
     // 가게 숨김 처리
+    @RoleCheck({UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @PatchMapping("/{storeId}/hide")
     public ResponseEntity<StoreResponseDTO> hideStore(@PathVariable UUID storeId) {
         StoreResponseDTO storeResponseDTO = storeService.hideStore(storeId); // 서비스에서 반환받은 가게 정보
@@ -81,6 +89,7 @@ public class StoreController {
     }
 
     //가게 복구
+    @RoleCheck({UserRoleEnum.MANAGER, UserRoleEnum.MASTER})
     @PatchMapping("/{storeId}/restore")
     public ResponseEntity<StoreResponseDTO> restoreStore(@PathVariable UUID storeId) {
         StoreResponseDTO storeResponseDTO = storeService.restoreStore(storeId); // 서비스에서 복구된 가게 정보
