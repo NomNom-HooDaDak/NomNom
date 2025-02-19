032efb3df8f83df137eedc8af8c3a89961eea828
poorm <315luz10@gmail.com>
2025-02-20 01:29:22 +0900

    [refactor] 리뷰엔티티 수정
    - 정적메서드, 셋터 역할 메서드 구현
    - 필요없는 필드 및 옵션 정리

--- src/main/java/com/p1/nomnom/reviews/entity/Review.java
+++ src/main/java/com/p1/nomnom/reviews/entity/Review.java
@@ -1,15 +1,18 @@
 package com.p1.nomnom.reviews.entity;
 
 import com.p1.nomnom.common.entity.BaseEntity;
+import com.p1.nomnom.orders.entity.Status;
 import com.p1.nomnom.reviewImages.entity.ReviewImage;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "p_reviews")
@@ -26,6 +29,15 @@ public class Review extends BaseEntity {
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Column(name = "store_id", nullable = false)
+    private UUID storeId;
+
     @Column(nullable = false)
     private int score;
 
@@ -33,9 +45,36 @@ public class Review extends BaseEntity {
     private String content;
 
     @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Column(nullable = false)
     private List<ReviewImage> reviewImages = new ArrayList<>();
 
-    @Column(name = "order_id", nullable = false)
-    private UUID orderId;
+    private boolean hidden;
+
+    public void addReviewImage(ReviewImage reviewImage) {
+        this.reviewImages.add(reviewImage);
+        reviewImage.setReview(this);
+    }
+
+    public static Review create(Long userId, String userName, UUID orderId, UUID storeId, int score, String content, List<ReviewImage> imageUrls) {
+        return new Review(
+                null,
+                userId,
+                userName,
+                orderId,
+                storeId,
+                score,
+                content,
+                imageUrls,
+                false
+        );
+    }
+
+    public void update(int score, String content) {
+        this.score = score;
+        this.content = content;
+    }
+
+    public void cancel(String deletedBy) {
+        markAsDeleted(deletedBy);
+        this.hidden = true;
+    }
 }
