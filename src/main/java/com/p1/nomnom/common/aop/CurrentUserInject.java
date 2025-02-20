beb46e14b926a63eaabcbcf6b5b5de6a285b5829
poorm <315luz10@gmail.com>
2025-02-20 12:30:05 +0900

    [refactor] CurrentUser 어노테이션 수정
    - 유저 객체로 받아오는 게 아닌 아이디, 네임, 롤, 객체자체 중 택해서 가져올 수 있습니다

--- src/main/java/com/p1/nomnom/common/aop/CurrentUserInject.java
+++ src/main/java/com/p1/nomnom/common/aop/CurrentUserInject.java
@@ -5,9 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 현재 로그인한 사용자 정보를 주입할 메서드에 사용
- */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CurrentUserInject {
---
+++ src/main/java/com/p1/nomnom/common/aop/UserContext.java
@@ -0,0 +1,18 @@
+package com.p1.nomnom.common.aop;
+
+
+import com.p1.nomnom.user.entity.User;
+import com.p1.nomnom.user.entity.UserRoleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class UserContext {
+    private Long userId;
+    private UserRoleEnum role;
+    private String username;
+    private User user;
+}
